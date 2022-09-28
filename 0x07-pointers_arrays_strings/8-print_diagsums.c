2dd605aaada995f483dc5edc360f903334ef4538
dEstInY-de-Gold <noahayebu@gmail.com>
2022-09-27 21:03:33 -0700

    Rocket out in space

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsum - prints sum diagonals in 2x2 array to stdout
+ * print_diagsums - prints sum diagonals in 2x2 array to stdout
  * @a: given 2d array
  * @size: size of array
  */
