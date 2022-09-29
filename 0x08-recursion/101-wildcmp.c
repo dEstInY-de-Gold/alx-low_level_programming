e2cffc4b299b1892deea2d47e0e76822b95f7eb0
dEstInY-de-Gold <noahayebu@gmail.com>
2022-09-28 22:25:02 -0700

    Life is sweet when God leads

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 1 if identical, 0 otherwise.
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+
+}
