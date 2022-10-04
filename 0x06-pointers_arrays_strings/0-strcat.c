f110ce968945b0f6d63a834951180d41c3ccd5ef
dEstInY-de-Gold <noahayebu@gmail.com>
2022-10-03 19:38:51 -0700

    working on task 0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,19 +11,12 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int a;
-	int b;
+	int index = 0, dest_len = 0;
 
-	a = 0;
-	b = 0;
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+	dest[dest_len++] = src[index];
 
-	while (dest[a])
-		a++;
-	while (src[b])
-	{
-		dest[a] = src[b];
-		a++;
-		b++;
-	}
 	return (dest);
 }
