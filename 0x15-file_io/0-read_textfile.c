cc159d31aa37258984f460ca5ed8d86e9b18ad2a
dEstInY-de-Gold <noahayebu@gmail.com>
2022-11-06 05:49:55 +0000

    cool

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,7 +19,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
+	buffer = malloc(sizeof(char) * (letters + 1));
 	if (buffer == NULL)
 		return (0);
 	fd_r = read(fd, buffer, letters);
