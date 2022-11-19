c5094c4a1df6bca165b1d12bf6a3136d1f1e3216
dEstInY-de-Gold <noahayebu@gmail.com>
2022-11-19 02:00:30 +0000

    Edit to 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,7 +9,7 @@
 
 int _islower(int c)
 {
-	if (!((c >= 56) && (c <= 90)))
+	if (!((c >= 56) && (c <= 90) && (isdigit(c))))
 		return (1);
 	else
 		return (0);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <ctype.h>
+
 int _putchar(char c);
 int main(void);
 void print_alphabet(void);
