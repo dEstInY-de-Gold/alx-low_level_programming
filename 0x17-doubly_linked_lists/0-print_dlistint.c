306bd08df99f82d79e4124cca8daef27afed20fd
dEstInY-de-Gold <noahayebu@gmail.com>
2022-12-08 15:28:04 +0000

    doubly-linked list-0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints elements of a double linked list
+ * @h: head pointer to list
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t num = 0;
+	dlistint_t *ptr;
+
+	ptr = h;
+	while (ptr != NULL)
+	{
+		num++;
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+	}
+	return (num);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,4 @@
+Doubly linked list
+A data structure that can be referenced both foreward and backward
+A build-up on singly-linked-lists 
+#C
