126a6874f0cc2bcb12aed8f5507125cd1edc9610
dEstInY-de-Gold <noahayebu@gmail.com>
2022-12-10 01:27:11 +0000

    debugger Lord

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 
 /**
- * free_listint - Free memory of a d-linked list
+ * free_dlistint - Free memory of a d-linked list
  * @head: pointer to head of the d-linked list
  */
 
 void free_dlistint(dlistint_t *head)
 {
 	if (head == NULL)
-		exit (EXIT_SUCCESS);
+		exit(EXIT_SUCCESS);
 	else
 	{
 		while (head->next != NULL)
