6354d01d3b252a5cdc9fa81be22e78ec84592801
Noah <noahayebu@gmail.com>
2023-07-10 20:29:20 +0000

    search algos

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,15 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - A linear search algorithm
+ * @array: A pointer to list of integers
+ * @size: size of Arrays
+ * @value: Value to search for
+ *
+ * Return: -1 on failure or the matched value
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int ptr
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,7 @@
+#ifndef SCH_H
+#define SCH_H
+
+int main(void);
+int linear_search(int *array, size_t size, int value);
+
+#endif
